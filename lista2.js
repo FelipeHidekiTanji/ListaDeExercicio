27cf7c9926028abbea1ddf1973e1c283542f693f
FelipeHidekiTanji <felipe.tanji@escola.pr.gov.br>
2026-02-27 13:11:56 +0000

    Resolução

--- lista2.js
+++ lista2.js
@@ -25,4 +25,10 @@ if (senha === 1234) {
     console.log(`Acesso negado`)
 }
 
-// 
+// Criar um progama que mostre um desconto de 5% para compras maiores que R$100.
+
+// Criar um progama que classifique uma pessoa como criança, adolecente ou adulto de acordo com a idade.
+
+// criar um progama que mostre qual tipo de tranangulo é diacordo com seus lados (Equilatero, Isóceles ou Escalneo).
+
+// Criar um progama que mostre conceitos A B C ou D e acordo com as notas: >9.5 A; Entre 9.4 e 8.5 B; Entre 8.4 e 7.0 C;
